9744b7d73529d8791a40f0b323184230cef1dec8
Tony Tanaka <tony.h.tanaka@gmail.com>
2020-10-19 14:23:47 +0200

    finished exercise

---
+++ tonytanaka/index.js
@@ -0,0 +1,20 @@
+// exercise 1
+let chrs = "ABCDEFGH";
+let count = chrs.length;
+let x = Math.floor(Math.random() * count) + 1;
+console.log(chrs[x]);
+
+// exercise 2
+let sy = "%&    § !* @= ";
+let cleanSy = sy.replace(/\s/g, "");
+/* \s is the regex for "whitespace", and g is the 
+"global" flag, meaning match ALL \s (whitespaces).*/
+let countSy = cleanSy.length;
+let y = Math.floor(Math.random() * countSy) + 1;
+console.log(cleanSy[y]);
+
+//exercise 3
+r = Math.floor(Math.random() * 255);
+g = Math.floor(Math.random() * 255);
+b = Math.floor(Math.random() * 255);
+console.log(`rgb (${r},${g}, ${b})`);
